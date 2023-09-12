0dbc72d93e655b6bbeba7dbc50713b83c04782a0
Ben Wallberg <wallberg@umd.edu>
2023-09-12 09:05:26 -0400

    Update version=1.0.10

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, University of Maryland Libraries'
 author = 'University of Maryland Libraries'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.8'
+release = '1.0.10'
 
 
 # -- General configuration ---------------------------------------------------
