468d7d649775c7fcea3b0c6aa96cdfd6632d62f4
Jeremy M Gottwig <jeremy.m.gottwig@gmail.com>
2025-04-08 10:06:06 -0400

    LIBWEB-6342. Adding libtools usage info to opendata.
    https://umd-dit.atlassian.net/browse/LIBWEB-6342

---
+++ source/code/website-libtools-hours.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import urllib.request
+import json
+
+# Retrieve McKeldin hours from libtools search
+
+BASE = 'https://api.www.lib.umd.edu/api/libtools/'
+ENDPOINT = 'mckeldin/hours/today'
+
+hours_url = BASE + ENDPOINT
+
+# Get search results as parsed JSON
+with urllib.request.urlopen(hours_url) as request:
+    response = json.loads(request.read())
+
+    status = response['status']
+
+    print('----')
+    print(f'McKeldin Hours: {status}')
