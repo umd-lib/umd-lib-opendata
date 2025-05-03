5b83876702e8094f784f6c6cce971b22c19a3def
Ben Wallberg <wallberg@umd.edu>
2025-05-03 15:18:30 -0400

    feat: begin drum-etd-stats

--- source/code/drum-etd-stats.py
+++ source/code/drum-etd-stats.py
@@ -40,50 +40,45 @@ def process_item(item):
 items_url = ENDPOINT + '/discover/search/objects?scope=ba3ddc3f-7a58-4fd3-bde5-304938050ea2&size=100'
 
 while items_url is not None:
-    print(items_url)
-    with urllib.request.urlopen(items_url) as request:
-        response = json.loads(request.read())
-        result = response['_embedded']['searchResult']
+    for backoff in [0,2,3,5,8,13,21,34,55,89,144,233,377,610,987]:
+        time.sleep(backoff)
+        try:
+            print(items_url)
+            with urllib.request.urlopen(items_url) as request:
+                response = json.loads(request.read())
+                result = response['_embedded']['searchResult']
 
-        # Iterate over the returned items
-        for item in result['_embedded']['objects']:
-            item = item['_embedded']['indexableObject']
-            md = item['metadata']
-            if 'dc.identifier.uri' in md:
-                link = "; ".join(entry['value'] for entry in md['dc.identifier.uri'])
-            else:
-                link = "n/a"
-            if 'dc.title' in md:
-                title = "; ".join(entry['value'] for entry in md['dc.title'])
-            else:
-                title = "n/a"
+                # Iterate over the returned items
+                for item in result['_embedded']['objects']:
+                    item = item['_embedded']['indexableObject']
+                    md = item['metadata']
+                    if 'dc.identifier.uri' in md:
+                        link = "; ".join(entry['value'] for entry in md['dc.identifier.uri'])
+                    else:
+                        link = "n/a"
+                    if 'dc.title' in md:
+                        title = "; ".join(entry['value'] for entry in md['dc.title'])
+                    else:
+                        title = "n/a"
 
-            print('----')
-            print(f'Title: {title}')
-            print(f'Link:  {link}')
+                    print('----')
+                    print(f'Title: {title}')
+                    print(f'Link:  {link}')
 
-            for backoff in [0,2,3,5,8,13,21,34,55,89,144,233,377,610,987]:
-                time.sleep(backoff)
-                try:
                     process_item(item)
-                    break
-                except urllib.error.HTTPError as e:
-                    print(f'HTTPError: {e}')
-                    # if e.code == 429:
-                    #     print('Rate limit exceeded, sleeping...')
-                    #     time.sleep(60)
-                    # else:
-                    #     raise
-                except Exception as e:
-                    print(f'Error: {e}')
-                    # raise
 
-        if 'next' in result['_links']:
-            items_url = result['_links']['next']['href']
-        else:
-            items_url = None
+                if 'next' in result['_links']:
+                    items_url = result['_links']['next']['href']
+                else:
+                    items_url = None
 
-    # break
+        except urllib.error.HTTPError as e:
+            print(f'HTTPError: {e}')
+            # if e.code == 429:
+            #     print('Rate limit exceeded, sleeping...')
+            #     time.sleep(60)
+            # else:
+            #     raise
 
 for year in sorted(stats.keys()):
     print(f'{year}, {stats[year]}')
